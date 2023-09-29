0b3432dd78cd53e82126644bcef89fcb4ef05b4e
Melchs40 <samscodingjourney@gmail.com>
2023-09-29 13:02:30 -0500

    Edit game functionality to properly declare winner in test.js

--- test.js
+++ test.js
@@ -1,22 +1,15 @@
+// creates an empty game board and stores the players' name and symbol
 const gameBoard = (() => {
     const gameBoardArr = ["", "", "", "", "", "", "", "", ""];
-    const square = document.getElementsByClassName("square");
-    const playerSymbol1 = "X";
-    const playerSymbol2 = "O";
     const players = [];
 
-    // for (let i = 0; i < square.length; i++) {
-    //     square[i].addEventListener("click", function changeBoard() {
-    //         square[i].textContent = gameBoardArr[i];
-    // })}; 
     return {
         gameBoardArr,
         players
     }
 })();
 
-
-
+// creates players and adds them to players array in gameBoard
 const Player = (name, playerSymbol) => {
 
     const sayHello = () => {
@@ -24,30 +17,20 @@ const Player = (name, playerSymbol) => {
     }
     gameBoard.players.push([name, playerSymbol]);
 
-    const changeBoard = () => {
-        let squares = document.querySelectorAll(".square");
-        let squaresArr = Array.from(squares);
-        squaresArr.forEach(item => {
-            item.addEventListener("click", function changeBoard() {
-                item.textContent = activePlayer[1];
-                item.classList.add(activePlayer[1]);
-                gameBoard.gameBoardArr[item.id] = activePlayer[1];
-                item.disabled = "true";
-                switchPlayerTurn();
-                document.getElementById("player-announce").textContent = `It is ${activePlayer[1]}'s turn`;
-            })
-        })
-    };
+    const startGame = () => {
+        playGame().changeBoard();
+        playGame().checkWinner();
+    }
 
     return {
         name,
         playerSymbol,
         sayHello: sayHello,
-        changeBoard: changeBoard
+        startGame: startGame
     }
 };
 
-
+// adds button to create players for game
 document.getElementById("submit-button").addEventListener("click", function createPlayer (){
     
     let playerName = document.getElementById("player-name").value.toLowerCase();
@@ -57,7 +40,6 @@ document.getElementById("submit-button").addEventListener("click", function crea
     console.log(playerSymbol);
     console.log(newPlayer);
 
-
     return {
         playerName,
         playerSymbol,
@@ -65,45 +47,64 @@ document.getElementById("submit-button").addEventListener("click", function crea
     }
 });
 
-let playerOne = Player("Player 1", "X");
-let playerTwo = Player("Player 2", "O");
-
-playerOne.changeBoard();
-
-let activePlayer = gameBoard.players[0];
-const switchPlayerTurn = () => {
-    activePlayer = activePlayer === gameBoard.players[0] ? gameBoard.players[1] : gameBoard.players[0];
-  };
-
-
-
+// creates the flow of the game
 playGame = () => {
     
     let activePlayer = gameBoard.players[0];
     const switchPlayerTurn = () => {
         activePlayer = activePlayer === gameBoard.players[0] ? gameBoard.players[1] : gameBoard.players[0];
       };
+    
+    const changeBoard = () => {
+        let squares = document.querySelectorAll(".square");
+        let squaresArr = Array.from(squares);
+        squaresArr.forEach(item => {
+            item.addEventListener("click", function changeBoard() {
+                item.textContent = activePlayer[1];
+                item.classList.add(activePlayer[1]);
+                gameBoard.gameBoardArr[item.id] = activePlayer[1];
+                item.disabled = "true";
+                checkWinner();
+                switchPlayerTurn();
+                document.getElementById("player-announce").textContent = `It is ${activePlayer[1]}'s turn`;
+            })
+        })
+    };
 
-    // const changeBoard = () => {
-    //     let squares = document.querySelectorAll(".square");
-    //     let squaresArr = Array.from(squares);
-    //     squaresArr.forEach(item => {
-    //         item.addEventListener("click", function changeBoard() {
-    //             item.textContent = activePlayer.symbol;
-    //             item.disabled = "true";
-    //             switchPlayerTurn();
-    //         })
-    //     })
-    // };
+    const checkWinner = () => {
+        const winners = [
+            [gameBoard.gameBoardArr[0], gameBoard.gameBoardArr[1], gameBoard.gameBoardArr[2]],
+            [gameBoard.gameBoardArr[3], gameBoard.gameBoardArr[4], gameBoard.gameBoardArr[5]],
+            [gameBoard.gameBoardArr[6], gameBoard.gameBoardArr[7], gameBoard.gameBoardArr[8]],
+            [gameBoard.gameBoardArr[0], gameBoard.gameBoardArr[3], gameBoard.gameBoardArr[6]], 
+            [gameBoard.gameBoardArr[1], gameBoard.gameBoardArr[4], gameBoard.gameBoardArr[7]],
+            [gameBoard.gameBoardArr[2], gameBoard.gameBoardArr[5], gameBoard.gameBoardArr[8]],
+            [gameBoard.gameBoardArr[0], gameBoard.gameBoardArr[4], gameBoard.gameBoardArr[8]],
+            [gameBoard.gameBoardArr[2], gameBoard.gameBoardArr[4], gameBoard.gameBoardArr[6]]
+        ];
+ 
+        for (let i = 0; i < winners.length; i++) {
+            if ((winners[i][0] === gameBoard.players[0][1] || winners[i][0] === gameBoard.players[1][1]) && winners[i][0] === winners[i][1] && winners[i][0] === winners[i][2]) {
+                alert(`${activePlayer[1]} has won the game!`)
+            } else {
+                console.log("nope")
+            }
+        };
+
+    }
 
     return {
         activePlayer,
         switchPlayerTurn: switchPlayerTurn,
-        // changeBoard: changeBoard
+        changeBoard: changeBoard,
+        checkWinner: checkWinner
     }
 };
 
-let game = playGame();
+let playerOne = Player("Player 1", "X");
+let playerTwo = Player("Player 2", "O");
+
+playerOne.startGame();
 
 
 
