5b4c32641ed764c57ab1e3bc7116197611af8108
Melchs40 <samscodingjourney@gmail.com>
2023-10-03 22:27:09 -0500

    Add win/draw functionality to game board using style.css, test.js

--- test.js
+++ test.js
@@ -64,30 +64,45 @@ playGame = () => {
                 item.classList.add(activePlayer[1]);
                 gameBoard.gameBoardArr[item.id] = activePlayer[1];
                 item.disabled = "true";
+                document.getElementById("player-announce").textContent = `It is ${activePlayer[1]}'s turn`;
                 checkWinner();
                 switchPlayerTurn();
-                document.getElementById("player-announce").textContent = `It is ${activePlayer[1]}'s turn`;
             })
         })
     };
 
     const checkWinner = () => {
+        const square = [];
+        let squares = document.querySelectorAll(".square");
+        let squaresArr = Array.from(squares);
+        let checkDisabled = (item) => item.disabled;
+
+        for (let i = 0; i < gameBoard.gameBoardArr.length; i++) {
+            square[i] = gameBoard.gameBoardArr[i];
+        }
+
         const winners = [
-            [gameBoard.gameBoardArr[0], gameBoard.gameBoardArr[1], gameBoard.gameBoardArr[2]],
-            [gameBoard.gameBoardArr[3], gameBoard.gameBoardArr[4], gameBoard.gameBoardArr[5]],
-            [gameBoard.gameBoardArr[6], gameBoard.gameBoardArr[7], gameBoard.gameBoardArr[8]],
-            [gameBoard.gameBoardArr[0], gameBoard.gameBoardArr[3], gameBoard.gameBoardArr[6]], 
-            [gameBoard.gameBoardArr[1], gameBoard.gameBoardArr[4], gameBoard.gameBoardArr[7]],
-            [gameBoard.gameBoardArr[2], gameBoard.gameBoardArr[5], gameBoard.gameBoardArr[8]],
-            [gameBoard.gameBoardArr[0], gameBoard.gameBoardArr[4], gameBoard.gameBoardArr[8]],
-            [gameBoard.gameBoardArr[2], gameBoard.gameBoardArr[4], gameBoard.gameBoardArr[6]]
+            [square[0], square[1], square[2]],
+            [square[3], square[4], square[5]],
+            [square[6], square[7], square[8]],
+            [square[0], square[3], square[6]], 
+            [square[1], square[4], square[7]],
+            [square[2], square[5], square[8]],
+            [square[0], square[4], square[8]],
+            [square[2], square[4], square[6]]
         ];
  
         for (let i = 0; i < winners.length; i++) {
             if ((winners[i][0] === gameBoard.players[0][1] || winners[i][0] === gameBoard.players[1][1]) && winners[i][0] === winners[i][1] && winners[i][0] === winners[i][2]) {
-                alert(`${activePlayer[1]} has won the game!`)
-            } else {
-                console.log("nope")
+                document.getElementById("player-announce").textContent = `${activePlayer[0]} has won the game!`;
+                document.getElementById("player-announce").classList.add("winner");
+                squaresArr.forEach(item => {
+                    item.disabled = "true";
+                })
+                break;
+            } else if (squaresArr.every(checkDisabled)) {
+                document.getElementById("player-announce").textContent = "The game is a draw";
+                break;
             }
         };
 
